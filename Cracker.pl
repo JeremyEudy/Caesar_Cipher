69ffeee8425eecce617864d98f50fea1c1db7a1e
Jeremy Eudy <jeremyeudy@gmail.com>
2018-09-13 09:08:30 -0400

    Fixed UI in Cracker.pl

--- Cracker.pl
+++ Cracker.pl
@@ -6,7 +6,7 @@
 #    By: jeudy2552 <jeudy2552@floridapoly.edu>          |  \`-\   \ |  o       #
 #                                                       |---\  \   `|  l       #
 #    Created: 2018/09/05 14:13:01 by jeudy2552          | ` .\  \   |  y       #
-#    Updated: 2018/09/12 14:43:57 by jeudy2552          -------------          #
+#    Updated: 2018/09/13 09:07:59 by jeudy2552          -------------          #
 #                                                                              #
 # **************************************************************************** #
 #!/usr/bin/perl -w
@@ -17,8 +17,10 @@ use Data::Dumper;
 use List::UtilsBy qw(max_by);
 
 my @alpha = ('A' .. 'Z');
+INPUT:
 print "Welcome to the Caesar cipher cracker.\nWould you like to import a file or input ciphertext manually?\n1 - File Import\n2 - User Input\n>";
 my $userInput = <>;
+system('clear');
 if($userInput == 1){
     my $fileName = <>;
     #Open file for read only
@@ -40,8 +42,24 @@ if($userInput == 1){
 }
 
 elsif($userInput == 2){
-    #TODO: This
+    print "Please input ciphertext: ";
+    my $content = <>;
+    $content = uc $content;                                        #Remove case sensitivity
+    $content =~ s/[^a-zA-Z]//g;                                    #Remove non alpha from str
+    my @charArray = split(//, $content);                           #Split into char array
+    my %charHash = map {                                           #Generate hash from char array and count frequency of each character
+        my $search = $_;
+        $_ ne "\n" ?
+            ($search => scalar grep {$_ eq $search} @charArray) :
+            () } @charArray;
+
 }
+
+else{
+    print "Please input a proper response.\n";
+    goto INPUT;
+}
+
 print "Letter | Count | Frequency in File \n";
 print "-------+-------+-------------------\n";
 foreach (reverse sort {$charHash{$a} <=> $charHash{$b}} keys %charHash) {
@@ -65,4 +83,4 @@ print "Guessed Shift amount: $shift\n";
 open(my $fileData, "+>>", "plainText.txt") or die "Couldn't open file plainText.txt, $!";
 print $fileData $plainText;
 close $fileData;
-
+print "Cracked plaintext stored in plainText.txt\n";
